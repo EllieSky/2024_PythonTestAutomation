a4c4cfb2b738fc50151f088410393dd337b938e0
teodo <teodorovychlil@gmail.com>
2024-05-02 19:48:26 -0700

    lteod lesson2 branch in GIT

---
+++ examples/lesson_4.py
@@ -0,0 +1,42 @@
+class Person:
+    def __init__(self, name, salary):
+        self.name = name
+        self.net_worth = 0
+        self.hourly_salary = float(salary)
+        self.stuff = list()
+    def introduce_self(self):
+        print(f'My name is {self.name}')
+
+    def check_net_worth(self):
+        print(self.net_worth)
+        return self.net_worth
+        # print net worth
+        pass
+
+    def buy_something(self, thing):
+        # add to inventory
+        pass
+    def sell_something(self, thing):
+        # remove from inventory
+        pass
+    def do_work(self, hours):
+        # yes, you can also use multiplication
+        for h in range(int(hours)):
+            self.net_worth +=self.hourly_salary
+        # earn money
+        pass
+
+
+person1 = Person("Maria", '55')
+person2 = Person("Steve", '50')
+
+person2.introduce_self()
+person2.do_work(8)
+
+person1.introduce_self()
+person1.do_work(4)
+
+print('person 1: ')
+person1.check_net_worth()
+print('person 2: ')
+person2.check_net_worth()
