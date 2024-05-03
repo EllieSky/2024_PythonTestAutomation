93dbb107b2a2733aab80b71c2e5b3c6e942f10ba
LiSag2022 <lsagitovar@gmail.com>
2024-05-02 22:55:37 -0400

    Person class, lesson 4

---
+++ examples/lesson_4.py
@@ -0,0 +1,42 @@
+class Person:
+
+    def __init__(self,name,salary):
+        #self.person_name = name
+        self.name = name
+        self.networth =0
+        self.hourly_salary=float(salary)
+        self.stuff=list()
+
+    def introduce_self(self):
+        print(f'My name is {self.name}')
+
+    def check_networth(self):
+        print(self.networth)
+        return self.networth
+
+    def buy_something(self, thing):
+        #add to inventory
+        pass
+
+    def sell_something(self, thing):
+        #remove from inventory
+        pass
+
+    def do_work(self,hours):
+        #earn money
+        for h in range (int(hours)):
+            self.networth += self.hourly_salary
+
+person1=Person("Mariya",55)
+person2=Person("Steve",50)
+
+person2.introduce_self()
+person2.do_work(8)
+
+person1.introduce_self()
+person1.do_work(4)
+
+print('person 1: ')
+person1.check_networth()
+print('person 2:')
+person2.check_networth()
