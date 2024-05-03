3e2ddd3866e721be681f81a7fd5a311665bfcbe9
Ellie Yampolskaya <ellie@lula.is>
2024-05-03 15:17:14 -0400

    Lesson 4 - end of class source

--- examples/lesson_4.py
+++ examples/lesson_4.py
@@ -1,3 +1,5 @@
+import time
+
 
 class Person:
     def __init__(self, name, salary):
@@ -10,36 +12,51 @@ class Person:
     def introduce_self(self):
         print(f'My name is {self.name}')
 
-    def check_net_worth(self):
-        print(self.net_worth)
-        return self.net_worth
+    def report_net_worth(self):
+        print(f"{self.name}'s current net worth is: {self.net_worth}")
 
-    def buy_something(self, thing):
-        # add to inventory
-        pass
+    def buy_something(self, thing: dict):
+        item = thing['item']
+        price = thing['price']
+        self.stuff.append(item)
+        self.net_worth -= price
 
-    def sell_something(self, thing):
+    def sell_something(self, thing: dict):
         # remove from inventory
+        # increase net worth
         pass
 
-    def do_work(self, hours):
-        # yes, you can also you multiplication :)
+    def do_work(self, hours: int):
+        # yes, you can also use multiplication :)
+        ## time.sleep(hours)
+        ## self.net_worth += self.hourly_salary * hours
         for h in range(int(hours)):
+            time.sleep(1)
             self.net_worth += self.hourly_salary
 
 
+
+####  Create 2 instances of Person class
 person1 = Person("Maria", 55)
 person2 = Person("Steve", 50)
 
+####  Interact with one of the objects
 person2.introduce_self()
 person2.do_work(8)
 
+print('person 2: ')
+person2.report_net_worth()
+
+
+####  Interact with the other object
 person1.introduce_self()
 person1.do_work(4)
 
 print('person 1: ')
-person1.check_net_worth()
+person1.report_net_worth()
 
-print('person 2: ')
-person2.check_net_worth()
+person1.buy_something({'item': 'toy car', 'price': 65.99})
+print(person1.stuff)
+print(person1.net_worth)
 
+pass
