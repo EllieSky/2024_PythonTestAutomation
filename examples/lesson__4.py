750ef9cfc5d0257399bef590a77a67b5922c4d07
danielabm1 <daniela.bulimar1997@gmail.com>
2024-05-02 21:09:27 -0700

    demo branch creation in Git

--- examples/lesson__4.py
+++ examples/lesson__4.py
@@ -1,3 +1,5 @@
+import time
+
 class Person:
     def __init__(self, name, salary):
         self.net_worth = 0
@@ -13,6 +15,10 @@ class Person:
         return self.net_worth
 
     def buy_something(self, thing):
+        item = thing['item']
+        price = thing ['price']
+        self.stuff.append(item)
+        self.net_worth = self.net_worth - price
         pass
 
     def sell_something(self, thing):
@@ -20,21 +26,28 @@ class Person:
 
     def do_work (self, hours):
         for h in range (int(hours)):
+            time.sleep(1)
             self.net_worth += self.hourly_salary
 
 
 person1 = Person("Maria", 55)
 person2 = Person("Steve", 50)
 
-person2.introduce_self()
-person2.do_work(8)
-
 person1.introduce_self()
 person1.do_work(4)
 
 print('person 1: ')
-person1.do_work(8)
+person1.check_net_worth()
+
+person2.introduce_self()
+person2.do_work(8)
 
 print('person 2: ')
-person2.do_work(8)
+person2.check_net_worth()
+
+person1.buy_something({"item": 'toy car', 'price': 65.99})
+print(person1.stuff)
+print(person1.net_worth)
+# d
+pass
 
