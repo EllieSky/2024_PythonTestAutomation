78f6fc5fa987a5c90b14311406b761be42b08af7
marcelbm1 <marcelbulimar.19@gmail.com>
2024-05-02 21:04:59 -0700

    Demo of branch

--- example/Lesson_4.py
+++ example/Lesson_4.py
@@ -1,10 +1,12 @@
+import time
+
 
 class Person:
     def __init__(self, name, salary):
         self.name = name
         self.net_worth = 0
         self.hourly_salary = float(salary)
-        self.staff = list()
+        self.stuff = list()
 
 
     def introduce_self(self):
@@ -15,13 +17,17 @@ class Person:
         return self.net_worth
 
     def buy_something(self, thing):
-        pass
+        item = thing['item']
+        price = thing['price']
+        self.stuff.append(item)
+        self.net_worth -= price
 
     def sell_something(self, thing):
         pass
 
     def do_work(self, hours):
         for h in range(int(hours)):
+            time.sleep(1)
             self.net_worth += self.hourly_salary
 
 
@@ -31,6 +37,9 @@ person2 = Person("Steve", 50)
 person2.introduce_self()
 person2.do_work(8)
 
+print('person2: ')
+person2.check_net_worth()
+
 person1.introduce_self()
 person1.do_work(4)
 
@@ -38,5 +47,8 @@ person1.do_work(4)
 print('person1: ')
 person1.check_net_worth()
 
-print('person2: ')
-person2.check_net_worth()
+person1.buy_something({'item': 'toy car', 'price': 65.99})
+print(person1.stuff)
+print(person1.net_worth)
+
+pass
