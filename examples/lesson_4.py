6d8d036e27b6beddf792892b133278ecdad3cbe1
teodo <teodorovychlil@gmail.com>
2024-05-02 21:04:46 -0700

    lteod lesson4 branch in GIT

--- examples/lesson_4.py
+++ examples/lesson_4.py
@@ -1,9 +1,13 @@
+import time
+
+
 class Person:
     def __init__(self, name, salary):
         self.name = name
         self.net_worth = 0
         self.hourly_salary = float(salary)
         self.stuff = list()
+
     def introduce_self(self):
         print(f'My name is {self.name}')
 
@@ -14,15 +18,22 @@ class Person:
         pass
 
     def buy_something(self, thing):
+        item = thing['item']
+        price = float(thing['price'])
+        self.stuff.append(item)
+        self.net_worth -= price
         # add to inventory
         pass
+
     def sell_something(self, thing):
         # remove from inventory
         pass
+
     def do_work(self, hours):
         # yes, you can also use multiplication
         for h in range(int(hours)):
-            self.net_worth +=self.hourly_salary
+            time.sleep(1)
+            self.net_worth += self.hourly_salary
         # earn money
         pass
 
@@ -33,10 +44,15 @@ person2 = Person("Steve", '50')
 person2.introduce_self()
 person2.do_work(8)
 
+print('person 2: ')
+person2.check_net_worth()
+
 person1.introduce_self()
 person1.do_work(4)
 
 print('person 1: ')
 person1.check_net_worth()
-print('person 2: ')
-person2.check_net_worth()
+
+person1.buy_something({'item': 'toy car', 'price': '65.99'})
+print(person1.stuff)
+print(person1.net_worth)
