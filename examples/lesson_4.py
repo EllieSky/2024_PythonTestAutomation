2f04a6463f5e94e921f14e8ca4a58e4fff310089
liliia <lol@gmail.com>
2024-05-02 19:52:32 -0700

    lesson 4 creation in GIT

---
+++ examples/lesson_4.py
@@ -0,0 +1,45 @@
+
+class Person:
+    def __init__(self, name, salary):
+        self.person_name = name
+        self.check_net_worth = 0
+        self.hourly_salary = salary
+        self.stuff = list()
+
+    def introduce_self(self):
+
+        print(f'My name is {self.person_name}')
+
+    def check_net_worth(self):
+        print (self.net_worth)
+        return self.net_worth
+
+
+    def buy_something(self, thing):
+        # add to inventory
+        pass
+
+    def sell_something(self, thing):
+        # remove from inventory
+        pass
+
+    def do_work(self, hours):
+        # yes, you can use multiplication
+        for h in range(int(hours)):
+            self.net_worth += self.hourly_salary
+
+
+
+person1 = Person("Maria", 55)
+
+person2 = Person("Steve", 50)
+
+person2.introduce_self()
+person2.do_work(8)
+person1.introduce_self()
+
+
+print('person1: ')
+person1.check_net_worth()
+print('person2:')
+person2.check_net_worth()
