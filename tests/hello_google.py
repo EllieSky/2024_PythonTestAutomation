ea1867b4bad292dbc4867e0dd92d076a0acb0958
marcelbm1 <marcelbulimar.19@gmail.com>
2024-05-07 20:35:51 -0700

    Demo of branch

---
+++ tests/hello_google.py
@@ -0,0 +1,29 @@
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class MyTestCase(unittest.TestCase):
+    def test_google_hello(self):
+        browser = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()))
+        browser.get('http://www.google.com')
+        browser.find_element(By.NAME, 'q').send_keys('hello', Keys.RETURN)
+        time.sleep(2)
+
+        page_title = browser.title
+        self.assertEqual('hello - Google Search', page_title)
+
+        page_url = browser.current_url
+        self.assertIn('/search?q=hello', page_url)
+
+        top_link_text = browser.find_element(By.CSS_SELECTOR, '[role="link"] [role="heading"]').text
+        self.assertEqual('Hello', top_link_text)
+
+
+if __name__ == '__main__':
+    unittest.main()
