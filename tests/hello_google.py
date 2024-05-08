2fd1f4c4f0a656eef530b0541d1c40fb2d1ddc13
Dinara Black <dindblack@gmail.com>
2024-05-07 20:47:52 -0700

    Lesson 5. Hello to you too, Google!

---
+++ tests/hello_google.py
@@ -0,0 +1,31 @@
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+class SearchUsingGoogle(unittest.TestCase):
+    def test_google_hello(self):
+        str_input = 'hello'
+
+        browser = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()))
+        browser.get('http://www.google.com')
+        browser.find_element(By.NAME, 'q').send_keys(str_input, Keys.RETURN)
+
+        page_title = browser.title
+
+        self.assertEqual(f'{str_input} - Google Search', page_title)  # add assertion here
+        page_url = browser.current_url
+        self.assertIn(f'/search?q={str_input}', page_url)
+
+        top_link_text = browser.find_element(By.CSS_SELECTOR, '[role="link"] [role="heading"]').text
+        self.assertEqual(str_input.capitalize(), top_link_text)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
