84214111b17f376603ede7010193b20d7e5c2ce0
danielabm1 <daniela.bulimar1997@gmail.com>
2024-05-09 21:07:43 -0700

    Lesson 5 - intro to webdriver

---
+++ tests/login.py
@@ -0,0 +1,42 @@
+import time
+import unittest
+
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class LoginTests(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()))
+        self.browser.get('https://demoqa.com/login')
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_valid_login(self):
+        browser = self.browser
+        browser.find_element(By.ID,'userName').send_keys('EllieSky')
+        browser.find_element(By.ID,'password').send_keys('Password1!')
+        browser.find_element(By.ID,'login').location_once_scrolled_into_view
+        browser.find_element(By.ID,'login').click()
+
+        # after login we want to check that username is displayed
+        time.sleep(2)
+        self.assertEqual('elliesky', browser.find_element(By.ID, 'userName-value').text)
+        self.assertIn('/profile', browser.current_url)
+
+        btn_logout_text = browser.find_element(By.ID, 'submit').text
+        self.assertEqual('Log out', btn_logout_text)
+
+
+    def test_invalid_password(selfs):
+        pass
+
+    def test_no_password(selfs):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
