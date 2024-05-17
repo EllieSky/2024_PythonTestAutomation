9d5d2032185e54d43f7577c0aa6316ad7f0283fb
danielabm1 <daniela.bulimar1997@gmail.com>
2024-05-17 14:13:57 -0700

    demo branch creation in Git

--- tests/homework_bonus_lesson7_daniela.py
+++ tests/homework_bonus_lesson7_daniela.py
@@ -4,19 +4,17 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from fixture.base_fixture import AdminLoginFixture
 
-class EmployeeSearch(AdminLoginFixture):
+class EmployeeSearchHwk(AdminLoginFixture):
 
     def test_search_by_job_title(self):
         expected_job_title = "QA Manager"
 
         browser = self.browser
-        browser.get('http://hrm-online.portnov.com/')
-        time.sleep(2)
 
         browser.find_element(By.ID, 'txtUsername').send_keys('admin')
         browser.find_element(By.ID, 'txtPassword').send_keys('password')
         browser.find_element(By.ID, 'btnLogin').click()
-        time.sleep(2)  # Wait for login to complete
+        time.sleep(2)
 
         welcome_message = browser.find_element(By.ID, 'welcome').text
         self.assertIn('Welcome', welcome_message)
@@ -28,12 +26,9 @@ class EmployeeSearch(AdminLoginFixture):
 
         list_of_records = browser.find_elements(By.CSS_SELECTOR, '#resultTable>tbody>tr')
 
-        # Advanced:
         for row in list_of_records:
             displayed_job_title = row.find_element(By.CSS_SELECTOR, 'td:nth-child(5)').text
             self.assertEqual(expected_job_title, displayed_job_title)
 
-
 if __name__ == '__main__':
     unittest.main()
-
