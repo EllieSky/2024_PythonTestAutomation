a05cd20a1642c8794184ac49297cc460a0233d1f
Dinara Black <dindblack@gmail.com>
2024-05-21 09:10:11 -0700

    Hello to you too, Google!

--- tests/dtb_hw1_test.py
+++ tests/dtb_hw1_test.py
@@ -1,26 +1,64 @@
 import unittest
 
 
-def sum_numbers(num1, num2, num3=0.0):
-    result = num1 + num2 + num3
-    return result
+def count_occurrences(word, letter):
+    count = 0
+
+    start_index = 0
+
+    while True:
+
+        index = word.find(letter, start_index)
+
+        if index == -1:
+            break
+
+        count += 1
+
+        start_index = index + 1
+
+    return count
 
 
-class TestCalculator(unittest.TestCase):
-    def test_example(self):
-        self.assertNotEqual(True, False)
-        self.assertEqual(True, True)
+def count_occurrences(word, letter, case_sensitive=True):
+    count = 0
 
-    def test_add_int_numbers(self):
-        actual = sum_numbers(66, 71)
-        expected = 137
-        self.assertEqual(expected, actual)
+    if not case_sensitive:
+        word = word.lower()
 
-    def test_add_decimal_numbers(self):
-        self.assertEqual(15.2, sum_numbers(11.2, 4))
+        letter = letter.lower()
 
-    def test_add_3_numbers(self):
-        self.assertEqual(10, sum_numbers(5, 3.5, 1.5))
+    start_index = 0
+
+    while True:
+
+        index = word.find(letter, start_index)
+
+        if index == -1:
+            break
+
+        count += 1
+
+        start_index = index + 1
+
+    return count
+
+
+class TestCountOccurrences(unittest.TestCase):
+
+    def test_occurrences(self):
+        self.assertEqual(count_occurrences('cucumber', 'u'), 2)
+
+        self.assertEqual(count_occurrences('cucumber', 'x'), 0)
+
+        self.assertEqual(count_occurrences('Cucumber', 'c'), 1)
+
+    def test_case_insensitive(self):
+        self.assertEqual(count_occurrences('cucumber', 'U', case_sensitive=False), 2)
+
+        self.assertEqual(count_occurrences('Cucumber', 'c', case_sensitive=False), 2)
+
+        self.assertEqual(count_occurrences('Cucumber', 'Uc', case_sensitive=False), 1)
 
 
 if __name__ == '__main__':
