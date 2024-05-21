11b95b3a2415484c48648f7ba2fd47056467b7f6
Petro Cherkas <cherkas.petr20@gmail.com>
2024-05-21 12:43:47 -0700

    I hope it'll work - 1st commit

--- lesson_4/pcherkas_hw_lesson4.py
+++ lesson_4/pcherkas_hw_lesson4.py
@@ -21,20 +21,31 @@ class Person:
     def buy_something(self, thing):
         item = thing["item"]
         price = thing["price"]
-        self.stuff.append(item)
-        self.net_worth -=price
+        if (self.net_worth - price) > 0:
+
+            self.stuff.append(item)
+            self.net_worth -= price
+        else:
+            print("You do not have enough money")
         pass
 
     def sell_something(self, thing):
+        item = thing["item"]
+        price = thing["price"]
+        self.stuff.remove(item)
+        self.net_worth += price
         #remove from inventory
         pass
 
     def do_work(self, hours):
         #earn money
-        for h in range (int(hours)):
+        for h in range(int(hours)):
             time.sleep(1)
             self.net_worth += self.hourly_salary
         pass
+
+
+pass
 
 person1 = Person("Petro", 55)
 person1.introduce_self()
@@ -43,12 +54,17 @@ person2 = Person("Steve", 50)
 person2.introduce_self()
 person2.do_work(8)
 
-
 person1.check_net_worth()
 
 person2.check_net_worth()
 
-person2.buy_something({"item": "toy car", "price": 65.99})
+person2.buy_something({"item": "toy car", "price": 401})
 person2.buy_something({"item": "car", "price": 00.99})
+person2.buy_something({"item": "rav4", "price": 1.99})
+person2.buy_something({"item": "chevy", "price": 00.99})
+person2.check_net_worth()
+print(person2.stuff)
+
+person2.sell_something({"item": "chevy", "price": 6000})
 person2.check_net_worth()
 print(person2.stuff)
