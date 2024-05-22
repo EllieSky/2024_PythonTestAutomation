7fbb78c2eeeecbe1e3882159bc0883596be30b33
Ellie Yampolskaya <ellie@lula.is>
2024-05-21 23:53:29 -0400

    Lesson 8 - POM, WebDriverWait, HW7

--- fixtures/base_fixture.py
+++ fixtures/base_fixture.py
@@ -4,18 +4,26 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+
+from pages.login import LoginPage
 
 
 class AdminLoginFixture(unittest.TestCase):
+    welcome_message_element = (By.ID, 'welcome')
+
     def setUp(self):
         self.browser = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()))
         self.browser.get('http://hrm-online.portnov.com/')
-        browser = self.browser
-        browser.find_element(By.ID, 'txtUsername').send_keys('admin')
-        browser.find_element(By.ID, 'txtPassword').send_keys('password')
-        browser.find_element(By.ID, 'btnLogin').click()
-        time.sleep(2)
+        self.login_page = LoginPage(self.browser)
+        self.login_page.authenticate()
+        self.login_page.wait_for_successful_login()
+        self.wait = WebDriverWait(self.browser, 5)
+        # self.wait.until(EC.presence_of_element_located(self.welcome_message_element))
+        # OR
+        # self.wait.until(EC.url_contains('/pim/viewEmployeeList'))
 
     def tearDown(self):
         self.browser.quit()
