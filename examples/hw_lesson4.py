898093d93b3aaea973a550eb23f3098ff6980efb
BariSv095 <svetlbarilova@gmail.com>
2024-05-24 17:23:32 -0500

    lesson 4 homework

--- examples/hw_lesson4.py
+++ examples/hw_lesson4.py
@@ -11,7 +11,7 @@ class Person:
         print(f'My name is {self.name}')
 
     def report_net_worth(self):
-        print(f"{self.name}'s current net worth is: {self.net_worth}")
+        print(f"{self.name}'s current net worth is: {self.net_worth:.2f}")
 
     def buy_something(self, thing: dict):  # def buy_something() to only work when there is enough funds, otherwise it should return without executing the transaction.
         item = thing['item']
@@ -22,13 +22,13 @@ class Person:
         else:
             return
 
-    def sell_something(self, thing1: dict):
+    def sell_something(self, thing: dict):
         # The function should check if a Person's list of stuff contains the item, and sell it if yes, otherwise it should return without executing the transaction.
-        item1 = thing1 ['item1']
-        price1 = thing1 ['price1']
-        if thing1 in self.stuff:
-            self.stuff.remove(item1)
-            self.net_worth += price1
+        item = thing ['item']
+        price = thing ['price']
+        if item in self.stuff:
+            self.stuff.remove(item)
+            self.net_worth += price
         else:
             return
 
@@ -49,29 +49,32 @@ person2 = Person("Steve", 50, ['notebook', 'pencil', 'hat'])
 ####  Interact with one of the objects
 person2.introduce_self()
 person2.do_work(8)
-print('person 2: ')
+print('person 2: ', "Initial Steve's stuff is: ", person2.stuff)
 person2.report_net_worth()
+
+person2.buy_something({'item': 'toy car', 'price': 40.99})
+print("Steve's stuff ", person2.stuff)
+print(f"Steve's net_worth :{person2.net_worth:.2f}")
+
+person2.sell_something({'item': 'hat', 'price': 14.59})
+print("Steve's stuff ", person2.stuff)
+print(f"Steve's net_worth: {person2.net_worth:.2f}")
+
 
 ####  Interact with the other object
 person1.introduce_self()
 person1.do_work(4)
-print('person 1: ')
+print('person 1: ', "Initial Maria's stuff is: ", person1.stuff)
 person1.report_net_worth()
 
 person1.buy_something({'item': 'shoes', 'price': 400.99})
 print("Maria's stuff ", person1.stuff)
-print("Maria's net_worth ", person1.net_worth)
+print(f"Maria's net_worth: {person1.net_worth:.2f}")
 
-person2.buy_something({'item': 'toy car', 'price': 40.99})
-print("Steve's stuff ", person2.stuff)
-print("Steve's net_worth ", person2.net_worth)
-
-person1.sell_something({'item1': 'book', 'price1': 24.00})
+person1.sell_something({'item': 'book', 'price': 24.00})
 print("Maria's stuff ", person1.stuff)
-print("Maria's net_worth ", person1.net_worth)
+print(f"Maria's net_worth: {person1.net_worth:.2f}")
 
-person2.sell_something({'item1': 'hat', 'price1': 14.59})
-print("Steve's stuff ", person2.stuff)
-print("Steve's net_worth ", person2.net_worth)
+
 
 
