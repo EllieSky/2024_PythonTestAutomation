0a3de65960651bd7ebdd22518ae02e2ced5e4385
Ellie Yampolskaya <blackellie@yahoo.com>
2024-06-01 09:44:26 -0400

    lesson 11 - POM in tests (#14)

--- fixtures/base_fixture.py
+++ fixtures/base_fixture.py
@@ -1,16 +1,14 @@
 import time
 import unittest
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from webdriver_manager.chrome import ChromeDriverManager
 
 from lib.browser import get_browser
+from pages.add_employee import AddEmployee
 from pages.employee_list import EmployeeList
 from pages.login import LoginPage
+from pages.personal_details import PersonalDetails
 from tests import DEFAULT_WAIT, DOMAIN
 
 
@@ -28,9 +26,13 @@ class AdminLoginFixture(BrowserFixture):
 
     def setUp(self):
         super().setUp()
-        self.browser.get(DOMAIN)
+        # self.browser.get(DOMAIN)
         self.login_page = LoginPage(self.browser)
         self.employee_list = EmployeeList(self.browser)
+        self.add_employee = AddEmployee(self.browser)
+        self.personal_details = PersonalDetails(self.browser)
+
+        self.login_page.go_to_page()
         self.login_page.authenticate()
         self.login_page.wait_for_successful_login()
         # self.wait.until(EC.presence_of_element_located(self.welcome_message_element))
