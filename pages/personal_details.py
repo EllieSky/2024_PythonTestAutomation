17b74855eaab281b7496b3c5ccd2102dfb6a1c1c
Ellie Yampolskaya <ellie@lula.is>
2024-06-01 09:43:30 -0400

    lesson 11 - POM in tests

--- pages/personal_details.py
+++ pages/personal_details.py
@@ -1,5 +1,25 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+
 from pages.base_page import BasePage
+from tests import BASE_URL
 
 
 class PersonalDetails(BasePage):
-    pass
+    PAGE_URL = f'{BASE_URL}/pim/viewEmployee/empNumber/'
+
+    fld_first_name = (By.ID, 'personal_txtEmpFirstName')
+    fld_last_name = (By.ID, 'personal_txtEmpLastName')
+    fld_employee_id = (By.ID, 'personal_txtEmployeeId')
+
+    def go_to_page(self, employee_code):
+        self.browser.get(self.PAGE_URL + employee_code)
+
+    def get_first_name(self):
+        return self.wait.until(EC.presence_of_element_located(self.fld_first_name)).get_attribute('value')
+
+    def get_last_name(self):
+        return self.wait.until(EC.presence_of_element_located(self.fld_last_name)).get_attribute('value')
+
+    def get_emp_id(self):
+        return self.wait.until(EC.presence_of_element_located(self.fld_employee_id)).get_attribute('value')
