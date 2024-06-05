5c10e4b1caf468380e1e0a6e1402acb077b4e1ba
danielabm1 <daniela.bulimar1997@gmail.com>
2024-06-04 18:50:59 -0700

    lesson 10

--- examples/hello_google.py
+++ examples/hello_google.py
@@ -9,13 +9,20 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 class SearchUsingGoogle(unittest.TestCase):
     def test_google_hello(self):
+        str_input = 'hello'
+
         browser = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()))
         browser.get('http://www.google.com')
+        browser.find_element(By.NAME, 'q').send_keys(str_input, Keys.RETURN)
 
         page_title = browser.title
+        self.assertEqual(f'{str_input} - Google Search', page_title)
 
         page_url = browser.current_url
+        self.assertIn(f'/search?q={str_input}', page_url)
 
+        top_link_text = browser.find_element(By.CSS_SELECTOR, '[role="link"] [role="heading"]').text
+        self.assertEqual(str_input.capitalize(), top_link_text)
 
 
 if __name__ == '__main__':
--- examples/login.py
+++ examples/login.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -21,6 +22,7 @@ class LoginTests(unittest.TestCase):
         browser.find_element(By.ID, 'login').location_once_scrolled_into_view
         browser.find_element(By.ID, 'login').click()
 
+        time.sleep(3)
         # After login in, we want to check the expected username is displayed
         self.assertEqual('elliesky', browser.find_element(By.ID, 'userName-value').text)
 
@@ -29,14 +31,30 @@ class LoginTests(unittest.TestCase):
         btn_logout_txt = browser.find_element(By.ID, 'submit').text
         self.assertEqual('Log out', btn_logout_txt)
 
+    def test_no_password(self):
+        browser = self.browser
+        browser.find_element(By.ID, 'login').location_once_scrolled_into_view
+        browser.find_element(By.ID, 'login').click()
 
+        class_attr_value = browser.find_element(By.ID, 'password').get_attribute('class')
+        self.assertIn('is-invalid', class_attr_value)
 
     def test_invalid_password(self):
-        pass
+        browser = self.browser
+        browser.find_element(By.ID, 'userName').send_keys('EllieSky')
+        browser.find_element(By.ID, 'password').send_keys('Pasword1!')
+        browser.find_element(By.ID, 'login').location_once_scrolled_into_view
+        browser.find_element(By.ID, 'login').click()
+        time.sleep(1)
+        self.assertEqual('Invalid username or password!', browser.find_element(By.ID, 'name').text)
 
-    def test_no_password(self):
-        pass
+    def test_no_username(self):
+        browser = self.browser
+        browser.find_element(By.ID, 'login').location_once_scrolled_into_view
+        browser.find_element(By.ID, 'login').click()
 
+        class_attr_value = browser.find_element(By.ID, 'userName').get_attribute('class')
+        self.assertIn('is-invalid', class_attr_value)
 
 if __name__ == '__main__':
     unittest.main()
