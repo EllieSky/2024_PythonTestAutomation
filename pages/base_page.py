f4a0beed7ec5b7ac77ef83b5e49d5f41856ce0ce
Ellie Yampolskaya <ellie@lula.is>
2024-06-04 23:43:45 -0400

    Lesson 12 - HW Solution part 1 and 2

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
 from tests import DEFAULT_WAIT
 
@@ -8,7 +9,7 @@ class BasePage:
     page_header = (By.CSS_SELECTOR, '.head>h1')
 
     def __init__(self, browser):
-        self.browser = browser
+        self.browser: WebDriver = browser
         self.wait = WebDriverWait(browser, DEFAULT_WAIT)
 
     def go_to_page(self):
