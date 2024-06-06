c381d1f20131bf4660d41723a8301b4634fa8041
Petro Cherkas <cherkas.petr20@gmail.com>
2024-06-06 11:50:45 -0700

    I want to save it

--- pc_fixtures/base_fixture.py
+++ pc_fixtures/base_fixture.py
@@ -3,19 +3,25 @@ import unittest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
 
 
 class AdminLoginFixture(unittest.TestCase):
-    def setUp(self):
+    def setUp(self, username='admin', password='password'):
         self.browser = webdriver.Chrome(service=Service(executable_path=ChromeDriverManager().install()))
         self.browser.get('http://hrm-online.portnov.com/')
-        self.browser.find_element(By.CSS_SELECTOR, '#txtUsername').send_keys('admin')
-        self.browser.find_element(By.CSS_SELECTOR, '#txtPassword').send_keys('password')
+        self.browser.find_element(By.CSS_SELECTOR, '#txtUsername').send_keys(f'{username}')
+        self.browser.find_element(By.CSS_SELECTOR, '#txtPassword').send_keys(f'{password}')
         self.browser.find_element(By.CSS_SELECTOR, '#btnLogin').click()
+        self.wait = WebDriverWait(self.browser, 5)
+        self.wait.until(EC.presence_of_element_located(By.CSS_SELECTOR, '#welcome'))
+
 
     def tearDown(self):
         self.browser.quit()
+
 
 if __name__ == '__main__':
     unittest.main()
