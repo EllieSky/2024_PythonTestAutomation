ea8266c7c80df732740b66f46d3fa175e80d6a42
Ellie Yampolskaya <ellie@lula.is>
2024-06-11 23:15:49 -0400

    basic gherkin feature file

---
+++ features/steps/test_steps.py
@@ -0,0 +1,23 @@
+from behave import given, when, then
+
+@given('my word is {name}')
+@given('my last name is {name}')
+@given('my name is {name}')
+def get_input(context, name):
+    if hasattr(context, "input_name"):
+        context.input_name += name
+    else:
+        context.input_name = name
+
+
+@when('I count the letters in my word')
+@when('I count the letters in my name')
+def count_letters(context):
+    context.name_length = len(context.input_name)
+
+
+@then('there should be {letter_count} letters in total')
+def check_count(context, letter_count):
+    assert context.name_length == int(letter_count),\
+        (f'Expected name length to be {letter_count}'
+         f' characters, but it was {context.name_length}')
