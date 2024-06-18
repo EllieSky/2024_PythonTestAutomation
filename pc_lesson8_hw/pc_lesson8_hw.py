af8daa2723b7875d6866d0c92a9ee8abe438b0dd
Petro Cherkas <cherkas.petr20@gmail.com>
2024-06-18 10:54:15 -0700

    I want to save it

--- pc_lesson8_hw/pc_lesson8_hw.py
+++ pc_lesson8_hw/pc_lesson8_hw.py
@@ -15,10 +15,7 @@ from selenium.webdriver.support import expected_conditions as EC, wait
 class MyTestCase(AdminLoginFixture):
 
     def __init__(self, methodName: str = "runTest"):
-
         super().__init__(methodName)
-
-
 
     def test_add_person(self):
         self.wait = WebDriverWait(self.browser, 10)
@@ -27,20 +24,17 @@ class MyTestCase(AdminLoginFixture):
         last_name = person_random.last_name()
         login_page = LoginPage(self.browser)
         login_page.wait_for_login()
-        password = person_random.password()
-        username = first_name + person_random.zipcode()
-        print(password)
-        print(username)
+        self.password = person_random.password()
+        self.username = first_name + person_random.zipcode()
         self.browser.find_element(By.CSS_SELECTOR, '#btnAdd').click()
         self.browser.find_element(By.CSS_SELECTOR, '#firstName').send_keys(first_name)
         self.browser.find_element(By.CSS_SELECTOR, '#lastName').send_keys(last_name)
         employee_id = self.browser.find_element(By.CSS_SELECTOR, '#employeeId').get_attribute('value')
         self.browser.find_element(By.CSS_SELECTOR, '#chkLogin').click()
-        # username = (By.CSS_SELECTOR, '#user_name')
         login_page.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#user_name')))
-        self.browser.find_element(By.CSS_SELECTOR, '#user_name').send_keys(str(username))
-        self.browser.find_element(By.CSS_SELECTOR, '#user_password').send_keys(f'{password}')
-        self.browser.find_element(By.CSS_SELECTOR, '#re_password').send_keys(f'{password}')
+        self.browser.find_element(By.CSS_SELECTOR, '#user_name').send_keys(str(self.username))
+        self.browser.find_element(By.CSS_SELECTOR, '#user_password').send_keys(f'{self.password}')
+        self.browser.find_element(By.CSS_SELECTOR, '#re_password').send_keys(f'{self.password}')
         self.browser.find_element(By.CSS_SELECTOR, '#btnSave').click()
         self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#profile-pic > h1')))
 
@@ -48,18 +42,25 @@ class MyTestCase(AdminLoginFixture):
 
         self.assertEqual(first_last_name, f"{first_name} {last_name}")
 
-        print(employee_id)
         self.browser.find_element(By.CSS_SELECTOR, '#welcome').click()
         self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#welcome-menu > ul > li:nth-child(2) > a')))
         self.browser.find_element(By.CSS_SELECTOR, '#welcome-menu > ul > li:nth-child(2) > a').click()
 
-        self.assertEqual(True, self.browser.find_element(By.CSS_SELECTOR, '#welcome-menu > ul > li:nth-child(2) > a'))
+        self.wait.until(EC.url_contains('http://hrm-online.portnov.com/symfony/web/index.php/auth/login'))
+        has_login = self.browser.find_element(By.CSS_SELECTOR, '#btnLogin')
+        login_exist = False
+        if has_login:
+            login_exist = True
 
-        # select = Select(self.browser.find_element(By.CSS_SELECTOR, '#welcome'))
+        self.assertEqual(True, login_exist)
+        self.browser.find_element(By.CSS_SELECTOR, '#txtUsername').send_keys(f'{self.username}')
+        self.browser.find_element(By.CSS_SELECTOR, '#txtPassword').send_keys(f'{self.password}')
+        self.browser.find_element(By.CSS_SELECTOR, '#btnLogin').click()
+        self.wait.until(EC.url_contains('viewMyDetails'))
+        self.wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#welcome')))
 
-        # select.select_by_value('logout')
-        time.sleep(3)
-
+        created_user = self.browser.find_element(By.CSS_SELECTOR, '#welcome').text
+        self.assertEqual(f'Welcome {first_name}', created_user)
 
         pass
 
